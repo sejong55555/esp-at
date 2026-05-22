f3f152d355db0dd1577ddfff5e4bab4bc2e3996c
sejong55555 <122588886+sejong55555@users.noreply.github.com>
2026-05-22 16:10:39 +0900

    Implement delay for UART transmission over 1024 bytes
    Add delay after UART transmission for large data.

--- main/interface/uart/at_uart_task.c
+++ main/interface/uart/at_uart_task.c
@@ -41,6 +41,11 @@ static int32_t at_uart_write_data(uint8_t *data, int32_t len)
     }
 
     length = uart_write_bytes(g_at_cmd_port, (char *)data, len);
+
+    if (len >= 1024)
+        uart_wait_tx_done(g_at_cmd_port, pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(20));
+    }
     return length;
 }
 
